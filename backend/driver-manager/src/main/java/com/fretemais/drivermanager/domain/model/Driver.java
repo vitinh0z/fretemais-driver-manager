977a781ac46f9e3b17fd0e52de7b3bb161946ae6
vitinh0z <victor10.vg23@gmail.com>
2026-02-05 19:53:14 -0300

    refactor: refatora codigo para utilizar lombok para criação de construtores e adiciona @Table e @Entity na Driver

--- backend/driver-manager/src/main/java/com/fretemais/drivermanager/domain/model/Driver.java
+++ backend/driver-manager/src/main/java/com/fretemais/drivermanager/domain/model/Driver.java
@@ -1,27 +1,34 @@
 package com.fretemais.drivermanager.domain.model;
 
 import com.fretemais.drivermanager.domain.enums.VehicleType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
 
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "drivers")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Driver {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
     private String name;
+    @Enumerated(EnumType.STRING)
     private VehicleType vehicleType;
+    @CPF
+    private String cpf;
     private String cnh;
     private String phoneNumber;
     private String email;
     private boolean available;
-
-    private Driver (
-        String name, VehicleType vehicleType, String cnh, 
-        String phoneNumber, String email, boolean available
-    )
-    {
-
-        this.name = name;
-        this.vehicleType = vehicleType;
-        this.cnh = cnh;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.available = true;
-    }  
 }
