da96941a7b71be0daecb880a7baa1c2b787be306
Victor Gabriel <100782235+vitinh0z@users.noreply.github.com>
2026-02-08 13:09:42 -0300

    implementa configuracao do jwt e controller para autenticação (#26)

---
+++ backend/driver-manager/src/main/java/com/fretemais/drivermanager/application/dtos/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package com.fretemais.drivermanager.application.dtos;
+
+public record LoginRequestDTO(String username, String password) {
+}
--- backend/driver-manager/src/main/java/com/fretemais/drivermanager/infrastructure/security/JwtAuthenticationFilter.java
+++ backend/driver-manager/src/main/java/com/fretemais/drivermanager/infrastructure/security/JwtAuthenticationFilter.java
@@ -1,5 +1,51 @@
 package com.fretemais.drivermanager.infrastructure.security;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+import java.security.Security;
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = getTokenFromRequest(request);
+
+        if (token != null){
+            String username = jwtTokenProvider.validateToken(token);
+            if (username != null){
+                UserDetails userDetails = new User(username, "", Collections.emptyList());
+
+                var authentication = new UsernamePasswordAuthenticationToken(token, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
 }
